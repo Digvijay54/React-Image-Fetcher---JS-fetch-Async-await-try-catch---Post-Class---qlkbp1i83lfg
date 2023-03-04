3db076dc1199438a99f623722a7f2c07c7e15207
Digvijay <digvijaykumar54@gmail.com>
2023-03-04 19:21:12 +0530

    assign2

--- src/components/PhotoFrame.js
+++ src/components/PhotoFrame.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 export const PhotoFrame = (props) => {
-<<<<<<< HEAD
     // console.log('url nd title - ', props.url, props.title)
     return(
         <>
@@ -11,15 +10,4 @@ export const PhotoFrame = (props) => {
             </div>
         </>
     )
-=======
-    console.log('url and title-',props.url,props.title)
-   return(
-    <>
-        <div className='photoframe'>
-            <img src={props.url}/>
-            <div className='caption'>{props.title}</div>
-        </div>
-    </>
-   )
->>>>>>> 88829ea7e76fef53f130673dd4b9c0bd22ec4606
 }
